117d06fd13be47ecda2537daf1c97ae92ca416d2
jkingz <jfkrule@gmail.com>
2025-04-14 04:51:23 +0800

    feat: Add forgot password reset

--- frontend/app/password-reset/[uid]/[token]/page.tsx
+++ frontend/app/password-reset/[uid]/[token]/page.tsx
@@ -1,5 +1,30 @@
-const Page = () => {
-  return <div>Password reset token</div>;
+import { PasswordResetConfirmForm } from '@/components/forms';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Full Auth | Password Reset Confirm',
+  description: 'Full Auth password reset confirm page',
 };
 
-export default Page;
+interface Props {
+  params: {
+    uid: string;
+    token: string;
+  };
+}
+
+export default function Page({ params: { uid, token } }: Props) {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Reset your password
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <PasswordResetConfirmForm uid={uid} token={token} />
+      </div>
+    </div>
+  );
+}
