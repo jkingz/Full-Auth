b825382ba990055f1789b4fbebb40ec70769a4bc
jkingz <jfkrule@gmail.com>
2025-04-13 18:34:33 +0800

    feat: Add footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
         <Provider>
           <Setup />
           <Navbar />
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8">
-            {children}
-          </div>
+          <div className="min-h-screen w-screen">{children}</div>
           <Footer />
         </Provider>
       </body>
--- frontend/components/common/Footer.tsx
+++ frontend/components/common/Footer.tsx
@@ -1,5 +1,13 @@
-const Footer = () => {
-  return <div>Footer</div>;
-};
-
-export default Footer;
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 h-16">
+      <div className="h-full px-2">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-gray-400 text-xs">
+            &copy; 2025 Full Auth, Inc. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
